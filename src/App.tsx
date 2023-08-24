431c5f3b5b8011b41a189ec85f5096d17320b1cd
Masaki Kihara <masaki1224.1998@gmail.com>
2023-08-24 22:08:10 +0900

    hooks、buttonコンポーネントimport

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,8 @@
-import { useState } from "react";
+import { Button } from "./components/button";
+import { useTimer } from "./hooks/useTimer";
 
 function App() {
-  const [second, setSecond] = useState<string>("55");
-  const [min, setMin] = useState<string>("59");
-  const [hour, setHour] = useState<string>("00");
-
-  const startTime = async () => {
-    let secondOnePlace = Number(second.slice(1, 2));
-    let secondTenPlace = Number(second.slice(0, 1));
-    let minOnePlace = Number(min.slice(1, 2));
-    let minTenPlace = Number(min.slice(0, 1));
-    let hourOnePlace = Number(hour.slice(1, 2));
-    let hourTenPlace = Number(hour.slice(0, 1));
-    setInterval(() => {
-      if (`${secondTenPlace}${secondOnePlace}` < "59") {
-        secondOnePlace++;
-        if (secondOnePlace > 9) {
-          secondTenPlace++;
-          secondOnePlace = 0;
-        }
-      } else if (`${secondTenPlace}${secondOnePlace}` === "59") {
-        secondTenPlace = 0;
-        secondOnePlace = 0;
-        minOnePlace++;
-        if (minOnePlace > 9) {
-          minTenPlace++;
-          minOnePlace = 0;
-        }
-      }
-      if (`${minTenPlace}${minOnePlace}` > "59") {
-        minTenPlace = 0;
-        minOnePlace = 0;
-        hourOnePlace++;
-        if (hourOnePlace > 9) {
-          hourTenPlace++;
-          hourOnePlace = 0;
-        }
-      }
-      setSecond(`${secondTenPlace}${secondOnePlace}`);
-      setMin(`${minTenPlace}${minOnePlace}`);
-      setHour(`${hourTenPlace}${hourOnePlace}`);
-    }, 1000);
-  };
-
-  const stopTime = () => {
-    console.log("stop");
-  };
-
-  const resetTime = () => {
-    setSecond("00");
-    setMin("00");
-    setHour("00");
-  };
-
+  const { second, min, hour, startTime, stopTime, resetTime } = useTimer();
   return (
     <>
       <div className="flex justify-center bg-slate-500 p-[20px]">
@@ -60,24 +10,9 @@ function App() {
       </div>
       <p className="text-center pt-10 text-9xl">{`${hour}:${min}:${second}`}</p>
       <div className="flex justify-center pt-10 gap-10">
-        <button
-          className="w-[200px] text-3xl bg-green-400 p-[10px] rounded"
-          onClick={startTime}
-        >
-          START
-        </button>
-        <button
-          className="w-[200px] text-3xl bg-red-500 rounded"
-          onClick={stopTime}
-        >
-          STOP
-        </button>
-        <button
-          className="w-[200px] text-3xl bg-yellow-300 rounded"
-          onClick={resetTime}
-        >
-          RESET
-        </button>
+        <Button buttonName={"START"} timerFn={startTime} />
+        <Button buttonName={"STOP"} timerFn={stopTime} />
+        <Button buttonName={"RESET"} timerFn={resetTime} />
       </div>
     </>
   );
